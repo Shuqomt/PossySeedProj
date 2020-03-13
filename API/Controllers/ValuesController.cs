d6ab1add43088b1484703b76f7542774deafb2ef
Tityana Shuqom <tityanas@exsilio.com>
2020-03-13 15:13:35 -0700

    Initial Commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            //query to db 
+            //synchronous version uses tolist()
+            //runs request to call our db on different thread and we await for task to be completed
+            //but we aren't waiting for the thread to be completed.
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            var value = await _context.Values.FindAsync(id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext //use this to query our db. can script a new db
+    { 
+        //create constructor
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Value> Values {get;set;}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    //initialize with some values
+                    new Value{
+                        Id = 1, 
+                        Name= "Value 101"}, 
+                        new Value{
+                        Id = 2, 
+                        Name= "Value 102"},
+                       new Value{
+                        Id = 3, 
+                        Name= "Value 103"}
+                    
+                );
+        }
+            // accessible to class its defined in any derived classes from this class
+    }
+}
